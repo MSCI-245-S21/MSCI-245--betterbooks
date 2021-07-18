e27d53f743c3192ed54a245128268ec7cad6848e
Dana Lee <d269lee@uwaterloo.ca>
2021-07-18 03:59:17 -0400

    app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord   
     validates_presence_of :name
     validates_length_of :name, maximum: 70    
-    validates_presence_of :email
+    #validates_presence_of :email, uniqueness: true
+    #validates :email, uniqueness: true
+    validates_uniqueness_of :email, :message => "This email is already taken. Try with another email"
     validates_length_of :email, maximum: 255
-    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
+    validates_format_of :email, with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
+    
+    before_save :lowercase_email
+    
+    
+    def lowercase_email
+        self.email= email.downcase.strip
+        
+    end
+        
+    
+
 end
